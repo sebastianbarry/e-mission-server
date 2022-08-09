4737215f3c4ce6e94783dd62d9717faa717c6157
Sebastian Barry <sebbarry@comcast.net>
2022-08-09 16:42:31 -0600

    Added "label percentage" section to check_participant_study.py
    check_participant_study.py
    - Added emission.core.wrapper.user to give it access to the computeConfirmed function
    - Added computeConfirmed and passed the user uuid as the parameter
    - Printed the results in the script

--- bin/monitor/check_participant_status.py
+++ bin/monitor/check_participant_status.py
@@ -1,4 +1,5 @@
 import emission.core.get_database as edb
+import emission.core.wrapper.user as ewu
 
 for ue in edb.get_uuid_db().find():
     trip_count = edb.get_analysis_timeseries_db().count_documents({"user_id": ue["uuid"], "metadata.key": "analysis/confirmed_trip"})
@@ -6,3 +7,5 @@ for ue in edb.get_uuid_db().find():
     last_trip = list(edb.get_analysis_timeseries_db().find({"user_id": ue["uuid"], "metadata.key": "analysis/confirmed_trip"}).sort("data.end_ts", -1).limit(1))
     last_trip_time = last_trip[0]["data"]["end_fmt_time"] if len(last_trip) > 0 else None
     print(f"For {ue['user_email']}: Trip count = {trip_count}, location count = {location_count}, last trip = {last_trip_time}")
+    confirmed_pct, valid_replacement_pct, score = ewu.User.computeConfirmed(ue)
+    print(f"for the last month: Confirmed label percentage = %{confirmed_pct}, Valid replacement label percentage = %{valid_replacement_pct}, Label score = %{score}")
